c7976e5adf7f32fb7226add810180f13a00b649e
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2022-03-18 16:12:06 +0000

    npm: add shortcut aliases

--- files/.profile.d/npm.sh
+++ files/.profile.d/npm.sh
@@ -3,6 +3,22 @@ if ! command -v npm >/dev/null; then
   return
 fi
 
+alias n='npm'
+alias ng='npm --global --no-audit'
+alias ngi='npm --global --no-audit install'
+alias ngu='npm --global --no-audit uninstall'
+alias ngup='npm --global --no-audit update'
+alias ni='npm install'
+alias nin='npm init'
+alias nid='npm install --save-dev'
+alias nr='npm run'
+alias nrb='npm rebuild'
+alias ns='npm start'
+alias nt='npm test'
+alias nu='npm uninstall'
+alias nud='npm uninstall --save-dev'
+alias nup='npm update'
+
 _npm_init_private_template='{
   "private": true,
   "type": "module"
