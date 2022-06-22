0918e870d71a340a6b319e60fd1398a32b21ca02
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2022-06-22 13:48:13 +0100

    python: add homebrew pythons to path if available

--- files/.profile.d/python.sh
+++ files/.profile.d/python.sh
@@ -3,9 +3,20 @@ if ! command -v python3 >/dev/null; then
   return
 fi
 
-export PYTHONDONTWRITEBYTECODE='true'
+export PYTHONDONTWRITEBYTECODE=1
 export PYTHONPYCACHEPREFIX="$HOME/.cache/python"
-export VIRTUALENVWRAPPER_PYTHON="$(which python3)"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
+export VIRTUALENVWRAPPER_PYTHON
+
+# Add all Homebrew Python versions into the PATH
+if command -v brew >/dev/null; then
+  for _python_path in "$(brew --prefix)"/opt/python@3.*/bin; do
+    if [ -d "$_python_path" ]; then
+      export PATH="$PATH:$_python_path"
+    fi
+  done
+  unset _python_path
+fi
 
 alias serve='python3 -m http.server'
+VIRTUALENVWRAPPER_PYTHON="$(which python3)"
