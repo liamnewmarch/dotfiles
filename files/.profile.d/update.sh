1f874d3b4ab313a3a3891d99089982e7ad4ed017
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2023-05-19 17:56:02 +0100

    update: don’t assume sudo, add env var checks to override defaults

--- files/.profile.d/update.sh
+++ files/.profile.d/update.sh
@@ -1,16 +1,36 @@
+_sudo_check() {
+  local _default _sudo_disabled _sudo_exists
+
+  _default=${1:-false}  # Whether to print sudo if other checks pass
+  _sudo_disabled=${NO_SUDO:-false}  # Whether the NO_SUDO flag has been set
+  _sudo_exists=$(command -v sudo >/dev/null)  # Whether sudo exists in PATH
+
+  if $_default && $_sudo_exists && ! $_sudo_disabled; then
+    printf 'sudo'
+  fi
+}
+
 update() {
+  local _sudo
+
+  # Update APT packages
   if command -v apt >/dev/null; then
-    sudo apt update
-    sudo apt --autoremove --quiet --yes upgrade
+    _sudo=$(_sudo_check "${SUDO_APT:-true}")
+    $_sudo apt update
+    $_sudo apt upgrade --autoremove --quiet --yes
   fi
 
+  # Update Homebrew packages
   if command -v brew >/dev/null; then
-    brew update
-    brew upgrade
-    brew cleanup --prune=all
+    _sudo=$(_sudo_check "${SUDO_BREW:-false}")
+    $_sudo brew update
+    $_sudo brew upgrade
+    $_sudo brew cleanup --prune=all
   fi
 
+  # Update global npm packges
   if command -v npm >/dev/null; then
-    npm -g update
+    _sudo=$(_sudo_check "${SUDO_NPM:-false}")
+    $_sudo npm -g update
   fi
 }
