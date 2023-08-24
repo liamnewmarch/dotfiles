9874390fa61396cb68f775adb2c94779b18aea9a
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2023-08-24 18:41:46 +0100

    python: better handling of unversioned Python from Homebrew

--- files/.profile.d/python.sh
+++ files/.profile.d/python.sh
@@ -8,14 +8,12 @@ export PYTHONPYCACHEPREFIX="$HOME/.cache/python"
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export VIRTUALENVWRAPPER_PYTHON
 
-# Add all Homebrew Python versions into the PATH
+# Add unversioned Homebrew Python to PATH
 if command -v brew >/dev/null; then
-  for _python_path in "$(brew --prefix)"/opt/python@3.*/bin; do
-    if [ -d "$_python_path" ]; then
-      export PATH="$PATH:$_python_path"
-    fi
-  done
-  unset _python_path
+  if [ -d "$(brew --prefix)/opt/python" ]; then
+    export PATH
+    PATH="$(brew --prefix python)/libexec/bin:$PATH"
+  fi
 fi
 
 alias serve='python3 -m http.server'
