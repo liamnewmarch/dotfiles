6fe94ac7986a4780b13e784e19155599cb579042
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2023-09-30 00:34:49 +0100

    nvm: remove homebrew lookup

--- files/.profile.d/nvm.sh
+++ files/.profile.d/nvm.sh
@@ -4,15 +4,8 @@ if [ -d "$NVM_DIR" ]; then
   # Lazy-load nvm
   nvm() {
     unset -f nvm
-    if command -v brew > /dev/null; then
-      _brew_nvm=$(brew --prefix nvm)
-      . "$_brew_nvm/nvm.sh"
-      . "$_brew_nvm/etc/bash_completion.d/nvm"
-      unset _brew_nvm
-    else
-      . "$NVM_DIR/nvm.sh"
-      . "$NVM_DIR/bash_completion"
-    fi
+    . "$NVM_DIR/nvm.sh"
+    . "$NVM_DIR/bash_completion"
     nvm "$@"
   }
 fi
