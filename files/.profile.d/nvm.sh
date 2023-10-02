cfa1774b7e81e3895847a2a22d019eba5b503206
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2023-10-02 10:25:32 +0100

    Don’t lazy load nvm and pyenv if they exist, performance is fine

--- files/.profile.d/nvm.sh
+++ files/.profile.d/nvm.sh
@@ -1,11 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
 
 if [ -d "$NVM_DIR" ]; then
-  # Lazy-load nvm
-  nvm() {
-    unset -f nvm
-    . "$NVM_DIR/nvm.sh"
-    . "$NVM_DIR/bash_completion"
-    nvm "$@"
-  }
+  . "$NVM_DIR/nvm.sh"
+  . "$NVM_DIR/bash_completion"
 fi
--- files/.profile.d/pyenv.sh
+++ files/.profile.d/pyenv.sh
@@ -1,10 +1,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 
 if [ -d "$PYENV_ROOT" ]; then
-  pyenv() {
-    unset -f pyenv
-    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-  }
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
 fi
