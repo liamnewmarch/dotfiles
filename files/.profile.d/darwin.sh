fdc20a91add98c4e94e8ad71ba0062c9466afcf5
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2024-05-08 16:11:22 +0100

    darwin: use SetFile instead of copying attributes with xattr

--- files/.profile.d/darwin.sh
+++ files/.profile.d/darwin.sh
@@ -1,5 +1,5 @@
-alias hidefile='xattr -w -x com.apple.FinderInfo "$(xattr -p -x com.apple.FinderInfo ~/Library)"'
-alias showfile='xattr -d com.apple.FinderInfo 2> /dev/null'
+alias hidefile='SetFile -a V'
+alias showfile='SetFile -a v'
 
 hyperlink() {
   printf "\e]8;;%s\e\\%s\e]8;;\e\\" "$@"
