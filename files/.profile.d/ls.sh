e794263d784dafc69715f73a15b90e4c4998dfe3
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2025-06-10 16:02:39 +0100

    ls: remove black backgrounds from LS_COLORS

--- files/.profile.d/ls.sh
+++ files/.profile.d/ls.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2153
 if [ -n "$IS_COLOR" ]; then
   export LSCOLORS='exfxcxdxbxegedabagacad'
-  export LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
+  export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
 
   if [ -n "$IS_MACOS" ]; then
     alias ls='ls -G'
