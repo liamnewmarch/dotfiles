1c968da87911f72915604a021642295fa069be39
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2026-01-01 11:13:34 +0000

    brew: remove unused aliases

--- files/.profile.d/brew.sh
+++ files/.profile.d/brew.sh
@@ -1,4 +1,3 @@
-# Homebrew is installed to /opt on arm64 which not in PATH
 if [ -d /opt/homebrew/bin ]; then
   export PATH="/opt/homebrew/bin:$PATH"
 fi
@@ -10,19 +9,3 @@ fi
 
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSTALL_UPGRADE=1
-
-alias bar='brew autoremove'
-alias bcl='brew cleanup --prune=all'
-alias bi='brew install'
-alias bl='brew leaves'
-alias bls='brew list'
-alias bo='brew outdated'
-alias bud='brew update'
-alias bun='brew uninstall'
-alias bup='brew upgrade'
-
-# Enable bash completions
-for _completion in "$(brew --prefix)/etc/bash_completion.d/"*; do
-  source "${_completion}"
-done
-unset _completion
