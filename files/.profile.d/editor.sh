0a4b6185708d8eb26336f19bddbf299656cba318
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2026-02-09 13:12:54 +0000

    editor: set $VISUAL, add sudoedit alias on macOS

--- files/.profile.d/editor.sh
+++ files/.profile.d/editor.sh
@@ -9,6 +9,15 @@ elif command -v vi >/dev/null; then
 fi
 
 if [ -n "$EDITOR" ]; then
+  export VISUAL="$EDITOR"
+
+  # The preferred way to edit files is to use the `edit` alias
   alias edit='$EDITOR'
-  alias vi='$EDITOR'
+
+  # The preferred way to edit files as root is to use `sudoedit`. This allows
+  # us to edit files with the user’s default editor and config. macOS doesn’t
+  # ship with sudoedit, so use an alias.
+  if ! command -v sudoedit >/dev/null; then
+    alias sudoedit='sudo -e'
+  fi
 fi
