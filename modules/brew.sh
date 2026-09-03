393c192074f8f601b6d6fbf874358ae41999e403
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2026-09-03 16:17:42 +0100

    brew: use shellenv

--- modules/brew.sh
+++ modules/brew.sh
@@ -11,5 +11,13 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSTALL_UPGRADE=1
 
 if [ -n "$BASH_VERSION" ]; then
-  try_source "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+  # Export env vars like HOMEBREW_PREFIX so `brew --prefix` isn’t necessary
+  eval "$(brew shellenv bash)"
+
+  # Load completions, adapted from https://docs.brew.sh/Shell-Completion#Bash
+  if ! try_source "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh"; then
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+      try_source "$COMPLETION"
+    done
+  fi
 fi
